cba299b3deccb13885aeae52d1ae7fd03a8d500e
Lorenzo Tettamanti <lorenzo.tettamanti5@gmail.com>
2024-10-18 21:07:09 +0200

    added blog design

--- src/components/Blog/components/blog_hero.jsx
+++ src/components/Blog/components/blog_hero.jsx
@@ -1,26 +1,33 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
-import "../../css/blog.css"
-
-import blogHeroImg from "../../assets/img/blog-hero-img.png"; // L'immagine di sfondo dell'hero
-
+import "../../css/blog.css";
+import HomeButton from "./home_btn";
+import blogHeroImg from "../../assets/img/blog-hero-img.png";
 
 const BlogHero = () => {
   return (
     <div>
-      <navBlog />
-      {/* Sezione Hero */}
+      {/* Verifica che il nome del componente sia corretto */}
       <section className="blog-hero-container">
-        <img src={blogHeroImg} alt="blog-hero-img" className="hero-bg" />
-        <div className="blog-title">
-          <h1>Stories on Innovation, Technology & IoT</h1>
-        </div>
-        <div className="blog-subtitle">
-          <h2>
-            Join our community and discover the latest insights, tutorials, and
-            breakthroughs in tech from the CortexFlow team
-          </h2>
+      <HomeButton />
+
+        <div className="row justify-content-center">
+          <div className="col-md-8">
+            {/* Contenuto centrato */}
+            <div className="blog-centering">
+              <img src={blogHeroImg} alt="blog-hero-img" className="hero-bg" />
+              <div className="blog-title">
+                <h1>Stories on Innovation, Technology & IoT</h1>
+              </div>
+              <div className="blog-subtitle">
+                <h2>
+                  Join our community and discover the latest insights,
+                  tutorials, and breakthroughs in tech from the CortexFlow team
+                </h2>
+              </div>
+            </div>
+          </div>
         </div>
       </section>
     </div>
---
+++ src/components/Blog/components/home_btn.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import "../../css/blog.css";
+
+
+const HomeButton=()=>{
+    return(
+        <>
+        {/* Bottone in alto a sinistra */}
+        <div className="button-home-container">
+          <Link to = "/blog">
+          <button className="btn-home-blog">
+            Home
+          </button>
+          </Link>
+        </div>
+        </>
+    );
+};
+export default HomeButton;
