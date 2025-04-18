b32ebc9ceae4cef3fab68f0d92c39cec0dc84f90
Lorenzo Tettamanti <lorenzo.tettamanti5@gmail.com>
2025-04-18 21:23:12 +0200

    added 3d design with animations

--- src/components/HomePage/home.jsx
+++ src/components/HomePage/home.jsx
@@ -7,6 +7,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "aos/dist/aos.css";
 import "glightbox/dist/css/glightbox.min.css";
 import "../css/homepage.css";
+import "../css/deco.css";
 import AOS from "aos"; // Importa AOS
 import { Helmet } from "react-helmet";
 
@@ -31,43 +32,6 @@ const Homepg = () => {
     </div>,
   ];
 
-  /* useEffect(() => {
-    // Verifica se l'utente è su un dispositivo mobile
-    const isMobile = window.matchMedia("(max-width: 768px)").matches;
-
-    // Intervallo in millisecondi: 10 secondi per desktop, 5 minuti per mobile
-    const changeInterval = isMobile ? 5 * 60 * 1000 : 10000;
-
-    const interval = setInterval(() => {
-      setCurrentComponent((prev) => (prev + 1) % components.length);
-    }, changeInterval);
-
-    return () => clearInterval(interval); // Pulizia dell'intervallo al momento della dismount
-  }, [components.length]);
-  useEffect(() => {
-    AOS.init(); // Inizializza AOS
-
-    // Opzioni per Typed.js
-    const options = {
-      strings: [
-        "A cutting-edge data simulation and big data analysis framework developed by CortexFlow, designed to simplify the simulation and the analysis of IoT devices.",
-      ],
-      showCursor: false,
-      typeSpeed: 50,
-      backSpeed: 25,
-      backDelay: 1000,
-      loop: false,
-    };
-
-    // Inizializza Typed.js
-    const typed = new Typed(typedRef.current, options);
-
-    // Cleanup
-    return () => {
-      typed.destroy();
-    };
-  }, []); // [] assicura che venga eseguito solo una volta al caricamento del componente
- */
   return (
     <>
       <Helmet>
@@ -87,159 +51,43 @@ const Homepg = () => {
       </Helmet>
       <Navbar />
       <main className="main">
-        <Spline scene="https://prod.spline.design/nTebIFs-JJgnfeDN/scene.splinecode" />
-
-        <div className="container section-title-about" data-aos="fade-up">
-          <h2>Key Features</h2>
-          <p>Explore CortexFlow key features</p>
+        <div className="deco-wrapper">
+          <div className="corner is--left-top"></div>
+          <div className="corner is--right-top"></div>
+          <div className="corner is--left-bottom"></div>
+          <div className="corner is--right-bottom"></div>
+        </div>
+        <div className="deco-h-lines">
+          <div className="decorative-line vertical-line" style={{ top: '0px', left: '250px' }}></div>
+          <div className="decorative-line vertical-line" style={{ top: '0px', left: '500px' }}></div>
+          <div className="decorative-line vertical-line" style={{ top: '0px', left: '750px' }}></div>
+          <div className="decorative-line vertical-line" style={{ top: '0px', left: '1000px' }}></div>
+          <div className="decorative-line vertical-line" style={{ top: '0px', left: '1250px' }}></div>
+          <div className="decorative-line vertical-line" style={{ top: '0px', left: '1500px' }}></div>
+          <div className="decorative-line vertical-line" style={{ top: '0px', left: '1750px' }}></div>
+          <div className="decorative-line vertical-line" style={{ top: '0px', left: '2000px' }}></div>
+        </div>
+        <div className="deco-plus">
+          <div className="decorative-plus" style={{ top: '200px', left: '500px' }}>+</div>
+          <div className="decorative-plus-medium" style={{ top: '200px', left: '1400px' }}>+</div>
+        
+          <div className="decorative-plus-back" style={{ top: '50px', left: '923px' }}>+</div>
+          <div className="decorative-plus" style={{ top: '650px', left: '802px' }}>+</div>
+        
         </div>
 
-        <section id="about" className="about section">
-          <div className="container" data-aos="fade-up">
-            <div className="row gx-5 gy-5">
-              {/* Prima funzionalità */}
-              <div
-                className="col-lg-6 d-flex flex-column"
-                data-aos="fade-up"
-                data-aos-delay="100"
-              >
-                <div className="feature-box">
-                  <h3>Unified Data Processing for IoT</h3>
-                  <p>
-                    Seamlessly unify the processing of batch and real-time
-                    streaming data. Leverage the power of the Python programming
-                    language for flexible and scalable data operations across
-                    different systems and diverse situations.
-                  </p>
-                </div>
-              </div>
-              {/* Seconda funzionalità */}
-              <div
-                className="col-lg-6 d-flex flex-column"
-                data-aos="fade-up"
-                data-aos-delay="200"
-              >
-                <div className="feature-box">
-                  <h3>Accelerated Analytics and Simulation</h3>
-                  <p>
-                    Run highly optimized queries across distributed data,
-                    supporting dashboarding, reporting, and ad-hoc analysis. Our
-                    engine is ready to outperform traditional data warehouses
-                    for swift insights. Run a simulation and simulate an
-                    environment faster than anyone.
-                  </p>
-                </div>
-              </div>
-              {/* Terza funzionalità */}
-              <div
-                className="col-lg-6 d-flex flex-column"
-                data-aos="fade-up"
-                data-aos-delay="300"
-              >
-                <div className="feature-box">
-                  <h3>Scalable Data Science</h3>
-                  <p>
-                    Conduct large-scale Exploratory Data Analysis (EDA) on large
-                    datasets without down sampling. Effortlessly explore data
-                    trends, data-driven simulations, and relationships across
-                    massive datasets using a high-performance analytical
-                    environment.
-                  </p>
-                </div>
-              </div>
-              {/* Quarta funzionalità */}
-              <div
-                className="col-lg-6 d-flex flex-column"
-                data-aos="fade-up"
-                data-aos-delay="400"
-              >
-                <div className="feature-box">
-                  <h3>Machine Learning at Scale</h3>
-                  <p>
-                    Leverage a unified data fabric to seamlessly train and scale
-                    machine learning models across decentralized clusters with
-                    thousands of nodes. Utilize the data mesh architecture to
-                    empower domain teams, enabling agile transitions from
-                    prototyping to production with fault-tolerant, distributed
-                    ML capabilities.
-                  </p>
-                </div>
-              </div>
-              {/* Quinta funzionalità */}
-              <div
-                className="col-lg-6 d-flex flex-column"
-                data-aos="fade-up"
-                data-aos-delay="500"
-              >
-                <div className="feature-box">
-                  <h3>Containerized Microservices for IoT</h3>
-                  <p>
-                    Leverage the power of Kubernetes and Docker to deploy,
-                    manage, and scale containerized microservices for IoT
-                    applications. Seamlessly integrate and orchestrate IoT
-                    sensors and edge devices across distributed environments,
-                    ensuring resilient, fault-tolerant operations.
-                  </p>
-                </div>
-              </div>
-            </div>
+        {/* Wrapper per la spline e il testo */}
+        <div className="content-wrapper">
+          {/* Testo di prova a sinistra */}
+          <div className="left-side-text">
+            <h1>CortexFlow</h1>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Inventore ipsa, quibusdam aspernatur odit neque a esse, eveniet, soluta vero facilis vel itaque amet. Quae consequatur repudiandae ut distinctio ducimus perspiciatis!</p>
           </div>
-        </section>
 
-        {/* <div className="container section-title-about" data-aos="fade-up">
-          <h2>Our Vision</h2>
-          <p>Roadmap and vision</p>
+          {/* Componente Spline */}
+          <Spline className="spline-wrapper" scene="https://prod.spline.design/nTebIFs-JJgnfeDN/scene.splinecode" />
         </div>
-        <section id="about" className="about section">
-          <div className="container" data-aos="fade-up">
-            <div className="row gx-0">
-              <div
-                className="col-lg-6 d-flex flex-column justify-content-center"
-                data-aos="fade-up"
-                data-aos-delay="200"
-              >
-                <div className="content">
-                  <h3>CortexFlow Technology</h3>
-                  <h2>
-                    Expedita voluptas omnis cupiditate totam eveniet nobis sint
-                    iste. Dolores est repellat corrupti reprehenderit.
-                  </h2>
-                  <p>
-                    Quisquam vel ut sint cum eos hic dolores aperiam. Sed
-                    deserunt et. Inventore et et dolor consequatur itaque ut
-                    voluptate sed et. Magnam nam ipsum tenetur suscipit
-                    voluptatum nam et est corrupti.
-                  </p>
-                  <div className="text-center text-lg-start">
-                    <Link
-                      to="#"
-                      className="btn-read-more d-inline-flex align-items-center justify-content-center align-self-center"
-                    >
-                      <span>Read More</span>
-                      <i className="bi bi-arrow-right"></i>
-                    </Link>
-                  </div>
-                </div>
-              </div>
-              <div
-                className="col-lg-6 d-flex align-items-center"
-                data-aos="zoom-out"
-                data-aos-delay="200"
-              >
-                <img src={roadmap} className="img-fluid" alt="About Us" />
-              </div>
-            </div>
-          </div>
-        </section>
-         */}
-        <section id="services" className="services section">
-          <div className="container section-title" data-aos="fade-up">
-            <h2>Technology</h2>
-            <p>Explore CortexFlow's best features and services</p>
-          </div>
 
-          <Slider />
-        </section>
         <Footer />
       </main>
     </>
--- src/components/navbar/navabar.jsx
+++ src/components/navbar/navabar.jsx
@@ -8,59 +8,53 @@ import "../css/homepage.css";
 
 import githubLogo from "../assets/img/github.png";
 
-import logobrain from "../assets/img/logo-brain.png";
-
+/* import logobrain from "../assets/img/logo-brain.png";
+ */
 const Homepg = () => {
   return (
     <header id="header" className="header d-flex align-items-center fixed-top">
-      <div className="container-fluid container-xl position-relative d-flex align-items-center">
-        <Link to="/" className="logo d-flex align-items-center me-auto">
-          <img
-            src={logobrain}
-            alt="brain Logo"
-            title="CortexFlow logo"
-            className="brain-logo"
-          />
-          <h2 className="sitename">CortexFlow</h2>
-        </Link>
-        <nav id="navmenu" className="navmenu">
-          <ul>
-            <li>
-              <Link to="/" className="active">
-                Home
-                <br />
-              </Link>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="https://blog.cortexflow.org/">
-                {" "}
-                {/* this must be a href */} <span>Blog</span>{" "}
-                <i className="bi toggle-dropdown"></i>
-              </a>
-            </li>
-            <li className="dropdown">
-              <a href="https://docs.cortexflow.org/">
-                {" "}
-                {/* this must be a href */} <span>Documentation</span>{" "}
-                <i className="bi toggle-dropdown"></i>
-              </a>
-            </li>
-          </ul>
-          <i className="mobile-nav-toggle d-xl-none bi bi-list"></i>
-        </nav>
+        <div className="container-fluid container-xl d-flex align-items-center justify-content-center">
+{/*           <Link to="/" className="logo d-flex align-items-center me-auto">
+            <h2 className="sitename">CortexFlow</h2>
+          </Link> */}
+          <nav id="navmenu" className="navmenu">
+            <ul>
+              <li>
+                <Link to="/" className="active">
+                  Home
+                  <br />
+                </Link>
+              </li>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="https://blog.cortexflow.org/">
+                  {" "}
+                  {/* this must be a href */} <span>Blog</span>{" "}
+                  <i className="bi toggle-dropdown"></i>
+                </a>
+              </li>
+              <li className="dropdown">
+                <a href="https://docs.cortexflow.org/">
+                  {" "}
+                  {/* this must be a href */} <span>Documentation</span>{" "}
+                  <i className="bi toggle-dropdown"></i>
+                </a>
+              </li>
+            </ul>
+            <i className="mobile-nav-toggle d-xl-none bi bi-list"></i>
+          </nav>
 
-        <Link
-          className="btn-getstarted flex-md-shrink-0"
-          to="https://github.com/CortexFlow/CortexBrain"
-          title="Get started with CortexFlow"
-        >
-          <img src={githubLogo} alt="GitHub Logo" className="github-logo" />
-          Get Started
-        </Link>
-      </div>
+          <Link
+            className="btn-getstarted flex-md-shrink-0"
+            to="https://github.com/CortexFlow/CortexBrain"
+            title="Get started with CortexFlow"
+          >
+            <img src={githubLogo} alt="GitHub Logo" className="github-logo" />
+            Get Started
+          </Link>
+        </div>
     </header>
   );
 };
