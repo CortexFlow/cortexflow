729669da9bf9d49569512344ed4fdfe866f3d639
Lorenzo Tettamanti <lorenzo.tettamanti5@gmail.com>
2025-06-28 00:13:50 +0200

    added tailwind integration

--- src/index.js
+++ src/index.js
@@ -4,8 +4,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 
-import router from "../src/components/Router/Router"; // Importa il router
+import router from "../src/components/Router/Router"; // Import the router
 
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
