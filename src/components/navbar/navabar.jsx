5f5e2ffba5760a26206e7735becf425e6489f72a
Lorenzo Tettamanti <lorenzo.tettamanti5@gmail.com>
2025-06-28 14:58:47 +0200

    navabar tailwindcss restyling

--- src/components/navbar/navabar.jsx
+++ src/components/navbar/navabar.jsx
@@ -1,67 +1,63 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap-icons/font/bootstrap-icons.css";
-import "aos/dist/aos.css";
-import "glightbox/dist/css/glightbox.min.css";
-import "../css/homepage.css";
-
 import githubLogo from "../assets/img/github.png";
-
 import logobrain from "../assets/img/logo-brain.png";
 
 const Homepg = () => {
   return (
-    <header id="header" className="header d-flex align-items-center fixed-top">
-      <div className="container-fluid container-xl position-relative d-flex align-items-center">
-        <Link to="/" className="logo d-flex align-items-center me-auto">
-          <img
-            src={logobrain}
-            alt="brain Logo"
-            title="CortexFlow logo"
-            className="brain-logo"
-          />
-          <h2 className="sitename">CortexFlow</h2>
+    <nav className="px-6 w-full bg-gradient-to-r from-gray-900 via-slate-800 to-gray-900 shadow-lg fixed z-50">
+      <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+        {/* Logo e nome */}
+        <Link to="/" className="flex items-center space-x-3">
+          {/*<img src={logobrain} alt="CortexFlow Logo" className="h-10 w-10" />*/}
+          <span className="text-2xl font-bold text-white tracking-wide">
+            CortexFlow
+          </span>
         </Link>
-        <nav id="navmenu" className="navmenu">
-          <ul>
-            <li>
-              <Link to="/" className="active">
-                Home
-                <br />
-              </Link>
-            </li>
-            <li>
-              <a href="#about">About</a>
-            </li>
-            <li>
-              <a href="https://blog.cortexflow.org/">
-                {" "}
-                {/* this must be a href */} <span>Blog</span>{" "}
-                <i className="bi toggle-dropdown"></i>
-              </a>
-            </li>
-            <li className="dropdown">
-              <a href="https://docs.cortexflow.org/">
-                {" "}
-                {/* this must be a href */} <span>Documentation</span>{" "}
-                <i className="bi toggle-dropdown"></i>
-              </a>
-            </li>
-          </ul>
-          <i className="mobile-nav-toggle d-xl-none bi bi-list"></i>
-        </nav>
 
+        {/* Navbar */}
+        <ul className="hidden md:flex space-x-8 text-sm font-medium text-gray-300">
+          <li>
+            <Link to="/" className="hover:text-white transition duration-300">
+              Home
+            </Link>
+          </li>
+          <li>
+            <a
+              href="#about"
+              className="hover:text-white transition duration-300"
+            >
+              About
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://blog.cortexflow.org/"
+              className="hover:text-white transition duration-300"
+            >
+              Blog
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://docs.cortexflow.org/"
+              className="hover:text-white transition duration-300"
+            >
+              Documentation
+            </a>
+          </li>
+        </ul>
+
+        {/* Get Started button */}
         <Link
-          className="btn-getstarted flex-md-shrink-0"
           to="https://github.com/CortexFlow/CortexBrain"
-          title="Get started with CortexFlow"
+          className="hidden md:inline-flex items-center bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg text-sm font-semibold shadow transition duration-300"
         >
-          <img src={githubLogo} alt="GitHub Logo" className="github-logo" />
+          <img src={githubLogo} alt="GitHub" className="h-4 w-4 mr-2" />
           Get Started
         </Link>
       </div>
-    </header>
+    </nav>
   );
 };
 
