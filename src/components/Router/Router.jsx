2e3b07bce3bc790c861bb44061fac1e97c6e0d66
Lorenzo Tettamanti <lorenzo.tettamanti5@gmail.com>
2025-06-28 10:07:48 +0200

    removed deprecated router routes

--- src/components/Router/Router.jsx
+++ src/components/Router/Router.jsx
@@ -1,16 +1,13 @@
-import React from "react";
 import { createBrowserRouter } from "react-router-dom";
 import HomePage from "../HomePage/home";
 import ErrorPage from "../Pages/ErrorPage";
-import InProgress from "../Pages/InProgress";
 const router = createBrowserRouter([
   {
     path: "/",
     element: <HomePage />,
     errorElement: <ErrorPage />,
   },
-  /* NewsLetter */
-  {
+  /*{
     path: "/examples",
     element: <InProgress />,
     errorElement: <ErrorPage />,
@@ -19,7 +16,7 @@ const router = createBrowserRouter([
     path: "/use-cases",
     element: <InProgress />,
     errorElement: <ErrorPage />,
-  },
+  },*/
 ]);
 
 export default router;
