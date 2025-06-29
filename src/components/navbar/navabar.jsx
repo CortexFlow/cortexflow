98c9c24c7bfabae6011bd7cf12a419a804754289
Lorenzo Tettamanti <lorenzo.tettamanti5@gmail.com>
2025-06-29 11:23:27 +0200

    Update navbar design

--- src/components/navbar/navabar.jsx
+++ src/components/navbar/navabar.jsx
@@ -1,31 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import githubLogo from "../assets/img/github.png";
-import logobrain from "../assets/img/logo-brain.png";
 
 const Homepg = () => {
   return (
     <nav className="px-6 w-full bg-gradient-to-r from-gray-900 via-slate-800 to-gray-900 shadow-lg fixed z-50">
-      <div className="max-w-7xl mx-auto px-6 py-4 flex items-center justify-between">
+      <div
+        className="max-w-7xl mx-auto md:px-6 py-4 flex items-center justify-between
+      sm:px-3"
+      >
         {/* Logo e nome */}
         <Link to="/" className="flex items-center space-x-3">
           {/*<img src={logobrain} alt="CortexFlow Logo" className="h-10 w-10" />*/}
-          <span className="text-2xl font-bold text-white tracking-wide">
+          <span className="text-2xl font-medium font-poppins text-white tracking-wide">
             CortexFlow
           </span>
         </Link>
 
         {/* Navbar */}
-        <ul className="hidden md:flex space-x-8 text-sm font-medium text-gray-300">
+        <ul className="font-poppins sm:flex md:flex space-x-8 text-sm font-medium text-gray-300">
           <li>
-            <Link to="/" className="hover:text-white transition duration-300">
+            <Link
+              to="/"
+              className="hover:text-blue-500 transition duration-300"
+            >
               Home
             </Link>
           </li>
           <li>
             <a
               href="#about"
-              className="hover:text-white transition duration-300"
+              className="hover:text-blue-500 transition duration-300"
             >
               About
             </a>
@@ -33,7 +38,7 @@ const Homepg = () => {
           <li>
             <a
               href="https://blog.cortexflow.org/"
-              className="hover:text-white transition duration-300"
+              className="hover:text-blue-500 transition duration-300"
             >
               Blog
             </a>
@@ -41,7 +46,7 @@ const Homepg = () => {
           <li>
             <a
               href="https://docs.cortexflow.org/"
-              className="hover:text-white transition duration-300"
+              className="hover:text-blue-500 transition duration-300"
             >
               Documentation
             </a>
@@ -51,7 +56,7 @@ const Homepg = () => {
         {/* Get Started button */}
         <Link
           to="https://github.com/CortexFlow/CortexBrain"
-          className="hidden md:inline-flex items-center bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg text-sm font-semibold shadow transition duration-300"
+          className="font-poppins hidden md:inline-flex items-center bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg text-sm font-medium shadow transition duration-300"
         >
           <img src={githubLogo} alt="GitHub" className="h-4 w-4 mr-2" />
           Get Started
