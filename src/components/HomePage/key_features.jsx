e20311d7796cba4d3f73263bbfd30e1d3d75b2ba
Lorenzo Tettamanti <lorenzo.tettamanti5@gmail.com>
2025-06-29 17:55:45 +0200

    added "key features" section

--- src/components/HomePage/key_features.jsx
+++ src/components/HomePage/key_features.jsx
@@ -3,114 +3,129 @@ import React from "react";
 const KeyFeatures = () => {
   return (
     <>
-      <div
-        class="container section-title-about font-poppins"
-        data-aos="fade-up"
-      >
-        <h2 class="font-poppins">Key Features</h2>
-        <p class="font-poppins">Explore CortexFlow key features</p>
-      </div>
-
-      <section id="about" className="about section">
-        <div className="container" data-aos="fade-up">
-          <div className="row gx-5 gy-5">
-            {/* Prima funzionalità */}
-            <div
-              class="col-lg-6 d-flex flex-column"
-              data-aos="fade-up"
-              data-aos-delay="100"
-            >
-              <div class="font-poppins feature-box">
-                <h3 class="font-poppins mb-3 text-xl">
-                  Unified Data Processing for IoT
-                </h3>
-                <p>
-                  Seamlessly unify the processing of batch and real-time
-                  streaming data. Leverage the power of the Python programming
-                  language for flexible and scalable data operations across
-                  different systems and diverse situations.
-                </p>
+      <div data-aos="fade-up">
+        <div class="bg-white lg:py-4 sm:py-32 md:py-2">
+          <div class="mx-auto lg:max-w-7xl px-6 lg:px-8 md:max-w-3xl md:px-4">
+            <div class="mx-auto max-w-7xl lg:text-center md:text-center">
+              <div class="container section-title" data-aos="fade-up">
+                <h2>eBPF Powered Technology</h2>
               </div>
+              <p class="w-full mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl lg:text-balance">
+                Everything you need to monitor your cluster
+              </p>
             </div>
-            {/* Seconda funzionalità */}
-            <div
-              class="col-lg-6 d-flex flex-column"
-              data-aos="fade-up"
-              data-aos-delay="100"
-            >
-              <div class="font-poppins feature-box">
-                <h3 class="font-poppins mb-3 text-xl">
-                  Accelerated Analytics and Simulation
-                </h3>
-                <p>
-                  Run highly optimized queries across distributed data,
-                  supporting dashboarding, reporting, and ad-hoc analysis. Our
-                  engine is ready to outperform traditional data warehouses for
-                  swift insights. Run a simulation and simulate an environment
-                  faster than anyone.
-                </p>
-              </div>
+            <div class="mx-auto mt-16 max-w-2xl sm:mt-20 lg:mt-24 lg:max-w-4xl">
+              <dl class="grid max-w-xl grid-cols-1 gap-x-8 gap-y-10 lg:max-w-none lg:grid-cols-2 lg:gap-y-16">
+                <div class="relative pl-16">
+                  <dt class="text-base/7 font-semibold text-gray-900">
+                    <div class="absolute left-0 top-0 flex size-10 items-center justify-center rounded-lg bg-yellow-400">
+                      <svg
+                        class="size-6 text-white"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                        data-slot="icon"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M12 16.5V9.75m0 0 3 3m-3-3-3 3M6.75 19.5a4.5 4.5 0 0 1-1.41-8.775 5.25 5.25 0 0 1 10.233-2.33 3 3 0 0 1 3.758 3.848A3.752 3.752 0 0 1 18 19.5H6.75Z"
+                        />
+                      </svg>
+                    </div>
+                    Networking
+                  </dt>
+                  <dd class="mt-2 text-base/7 text-gray-600">
+                    Morbi viverra dui mi arcu sed. Tellus semper adipiscing
+                    suspendisse semper morbi. Odio urna massa nunc massa.
+                  </dd>
+                </div>
+                <div class="relative pl-16">
+                  <dt class="text-base/7 font-semibold text-gray-900">
+                    <div class="absolute left-0 top-0 flex size-10 items-center justify-center rounded-lg bg-indigo-600">
+                      <svg
+                        class="size-6 text-white"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                        data-slot="icon"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M16.5 10.5V6.75a4.5 4.5 0 1 0-9 0v3.75m-.75 11.25h10.5a2.25 2.25 0 0 0 2.25-2.25v-6.75a2.25 2.25 0 0 0-2.25-2.25H6.75a2.25 2.25 0 0 0-2.25 2.25v6.75a2.25 2.25 0 0 0 2.25 2.25Z"
+                        />
+                      </svg>
+                    </div>
+                    Monitoring
+                  </dt>
+                  <dd class="mt-2 text-base/7 text-gray-600">
+                    Sit quis amet rutrum tellus ullamcorper ultricies libero
+                    dolor eget. Sem sodales gravida quam turpis enim lacus amet.
+                  </dd>
+                </div>
+                <div class="relative pl-16">
+                  <dt class="text-base/7 font-semibold text-gray-900">
+                    <div class="absolute left-0 top-0 flex size-10 items-center justify-center rounded-lg bg-green-500">
+                      <svg
+                        class="size-6 text-white"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                        data-slot="icon"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+                        />
+                      </svg>
+                    </div>
+                    Observability
+                  </dt>
+                  <dd class="mt-2 text-base/7 text-gray-600">
+                    Quisque est vel vulputate cursus. Risus proin diam nunc
+                    commodo. Lobortis auctor congue commodo diam neque.
+                  </dd>
+                </div>
+                <div class="relative pl-16">
+                  <dt class="text-base/7 font-semibold text-gray-900">
+                    <div class="absolute left-0 top-0 flex size-10 items-center justify-center rounded-lg bg-red-500">
+                      <svg
+                        class="size-6 text-white"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke-width="1.5"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                        data-slot="icon"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          d="M7.864 4.243A7.5 7.5 0 0 1 19.5 10.5c0 2.92-.556 5.709-1.568 8.268M5.742 6.364A7.465 7.465 0 0 0 4.5 10.5a7.464 7.464 0 0 1-1.15 3.993m1.989 3.559A11.209 11.209 0 0 0 8.25 10.5a3.75 3.75 0 1 1 7.5 0c0 .527-.021 1.049-.064 1.565M12 10.5a14.94 14.94 0 0 1-3.6 9.75m6.633-4.596a18.666 18.666 0 0 1-2.485 5.33"
+                        />
+                      </svg>
+                    </div>
+                    Advanced security
+                  </dt>
+                  <dd class="mt-2 text-base/7 text-gray-600">
+                    Arcu egestas dolor vel iaculis in ipsum mauris. Tincidunt
+                    mattis aliquet hac quis. Id hac maecenas ac donec pharetra
+                    eget.
+                  </dd>
+                </div>
+              </dl>
             </div>
-            {/* Terza funzionalità */}
-            <div
-              class="col-lg-6 d-flex flex-column"
-              data-aos="fade-up"
-              data-aos-delay="100"
-            >
-              <div class="font-poppins feature-box">
-                <h3 class="font-poppins mb-3 text-xl">Scalable Data Science</h3>
-                <p>
-                  Conduct large-scale Exploratory Data Analysis (EDA) on large
-                  datasets without down sampling. Effortlessly explore data
-                  trends, data-driven simulations, and relationships across
-                  massive datasets using a high-performance analytical
-                  environment.
-                </p>
-              </div>
-            </div>
-            {/* Quarta funzionalità */}
-            <div
-              class="col-lg-6 d-flex flex-column"
-              data-aos="fade-up"
-              data-aos-delay="100"
-            >
-              <div class="font-poppins feature-box">
-                <h3 class="font-poppins mb-3 text-xl">
-                  Machine Learning at Scale
-                </h3>
-                <p>
-                  Leverage a unified data fabric to seamlessly train and scale
-                  machine learning models across decentralized clusters with
-                  thousands of nodes. Utilize the data mesh architecture to
-                  empower domain teams, enabling agile transitions from
-                  prototyping to production with fault-tolerant, distributed ML
-                  capabilities.
-                </p>
-              </div>
-            </div>
-            {/* Quinta funzionalità */}
-            <div
-              class="col-lg-6 d-flex flex-column"
-              data-aos="fade-up"
-              data-aos-delay="100"
-            >
-              <div class="font-poppins feature-box">
-                <h3 class="font-poppins mb-3 text-xl">
-                  Containerized Microservices for IoT
-                </h3>
-                <p>
-                  Leverage the power of Kubernetes and Docker to deploy, manage,
-                  and scale containerized microservices for IoT applications.
-                  Seamlessly integrate and orchestrate IoT sensors and edge
-                  devices across distributed environments, ensuring resilient,
-                  fault-tolerant operations.
-                </p>
-              </div>
-            </div>
-            {/*chiusura*/}
           </div>
         </div>
-      </section>
+      </div>
     </>
   );
 };
