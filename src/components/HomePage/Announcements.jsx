f652ae91fbc47f5d1c00783b157770ab0efd4fa1
Lorenzo Tettamanti <lorenzo.tettamanti5@gmail.com>
2025-06-29 18:10:59 +0200

    added "Announcements tab"

---
+++ src/components/HomePage/Announcements.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Announcements = () => {
+  return (
+    <div className="max-w-7xl w-full mx-auto border-b border-gray-200 bg-gray-100 px-4 py-2 text-gray-900">
+      <p className="text-center font-medium">
+        ⭐ CortexBrain is currently in active development so occasional bugs may
+        occur ⭐
+        <a href="#" className="inline-block underline">
+          {" "}
+        </a>
+      </p>
+    </div>
+  );
+};
+
+export default Announcements;
