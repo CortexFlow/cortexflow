825cd31c472156c9d480035d0b8b6425c103705a
Lorenzo Tettamanti <lorenzo.tettamanti5@gmail.com>
2025-06-30 23:37:06 +0200

    updated FeatureShow component

--- src/components/HomePage/FeatureShow.jsx
+++ src/components/HomePage/FeatureShow.jsx
@@ -2,99 +2,113 @@ import React from "react";
 
 const FeatureShow = () => {
   return (
-    <div class="overflow-hidden bg-white py-24 sm:py-32">
-      <div class="mx-auto max-w-7xl px-6 lg:px-8">
-        <div class="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
-          <div class="lg:pr-8 lg:pt-4">
-            <div class="lg:max-w-lg">
-              <h2 class="text-base/7 font-semibold text-indigo-600">
+    <div className="overflow-hidden bg-white mt-20">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <div className="lg:pr-8 lg:pt-2">
+            <div className="lg:max-w-lg">
+              <h2 className="text-base/7 font-semibold text-indigo-600">
                 Enhanced Monitoring
               </h2>
-              <p class="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
-                A better workflow
+              <p className="mt-2 text-pretty text-4xl font-semibold tracking-tight text-gray-900 sm:text-5xl">
+                Kernel Level Insights
               </p>
-              <p class="mt-6 text-lg/8 text-gray-700">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                Maiores impedit perferendis suscipit eaque, iste dolor
-                cupiditate blanditiis ratione.
+              <p className="mt-6 text-lg/8 text-gray-700">
+                CortexBrain, the core CortexFlow component, leverage the power
+                of eBPF programs to easily monitor your cluster without
+                additional overhead
               </p>
-              <dl class="mt-10 max-w-xl space-y-8 text-base/7 text-gray-600 lg:max-w-none">
-                <div class="relative pl-9">
-                  <dt class="inline font-semibold text-gray-900">
-                    <svg
-                      class="absolute left-1 top-1 size-5 text-indigo-600"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      aria-hidden="true"
-                      data-slot="icon"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M5.5 17a4.5 4.5 0 0 1-1.44-8.765 4.5 4.5 0 0 1 8.302-3.046 3.5 3.5 0 0 1 4.504 4.272A4 4 0 0 1 15 17H5.5Zm3.75-2.75a.75.75 0 0 0 1.5 0V9.66l1.95 2.1a.75.75 0 1 0 1.1-1.02l-3.25-3.5a.75.75 0 0 0-1.1 0l-3.25 3.5a.75.75 0 1 0 1.1 1.02l1.95-2.1v4.59Z"
-                        clip-rule="evenodd"
-                      />
-                    </svg>
-                    Feature 1
-                  </dt>
-                  <dd class="inline">
-                    Lorem ipsum, dolor sit amet consectetur adipisicing elit.
-                    Maiores impedit perferendis suscipit eaque, iste dolor
-                    cupiditate blanditiis ratione.
-                  </dd>
-                </div>
-                <div class="relative pl-9">
-                  <dt class="inline font-semibold text-gray-900">
-                    <svg
-                      class="absolute left-1 top-1 size-5 text-indigo-600"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      aria-hidden="true"
-                      data-slot="icon"
-                    >
-                      <path
-                        fill-rule="evenodd"
-                        d="M10 1a4.5 4.5 0 0 0-4.5 4.5V9H5a2 2 0 0 0-2 2v6a2 2 0 0 0 2 2h10a2 2 0 0 0 2-2v-6a2 2 0 0 0-2-2h-.5V5.5A4.5 4.5 0 0 0 10 1Zm3 8V5.5a3 3 0 1 0-6 0V9h6Z"
-                        clip-rule="evenodd"
-                      />
-                    </svg>
-                    Feature 2
-                  </dt>
-                  <dd class="inline">
-                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure
-                    qui lorem cupidatat commodo.
-                  </dd>
-                </div>
-                <div class="relative pl-9">
-                  <dt class="inline font-semibold text-gray-900">
-                    <svg
-                      class="absolute left-1 top-1 size-5 text-indigo-600"
-                      viewBox="0 0 20 20"
-                      fill="currentColor"
-                      aria-hidden="true"
-                      data-slot="icon"
-                    >
-                      <path d="M4.632 3.533A2 2 0 0 1 6.577 2h6.846a2 2 0 0 1 1.945 1.533l1.976 8.234A3.489 3.489 0 0 0 16 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234Z" />
-                      <path
-                        fill-rule="evenodd"
-                        d="M4 13a2 2 0 1 0 0 4h12a2 2 0 1 0 0-4H4Zm11.24 2a.75.75 0 0 1 .75-.75H16a.75.75 0 0 1 .75.75v.01a.75.75 0 0 1-.75.75h-.01a.75.75 0 0 1-.75-.75V15Zm-2.25-.75a.75.75 0 0 0-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 0 0 .75-.75V15a.75.75 0 0 0-.75-.75h-.01Z"
-                        clip-rule="evenodd"
-                      />
-                    </svg>
-                    Feature 3
-                  </dt>
-                  <dd class="inline">
-                    Ac tincidunt sapien vehicula erat auctor pellentesque
-                    rhoncus. Et magna sit morbi lobortis.
-                  </dd>
-                </div>
-              </dl>
+              <ul className="mt-10 space-y-8 text-base/7 text-gray-600">
+                <li className="flex items-start gap-3">
+                  <svg
+                    className="w-5 h-5 mt-1 text-blue-800 flex-shrink-0"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="1.2"
+                      d="M12 15v5m-3 0h6M4 11h16M5 15h14a1 1 0 0 0 1-1V5a1 1 0 0 0-1-1H5a1 1 0 0 0-1 1v9a1 1 0 0 0 1 1Z"
+                    />
+                  </svg>
+                  <div>
+                    <strong className="font-semibold text-gray-900">
+                      Effortless Monitoring:
+                    </strong>{" "}
+                    Metrics, logs, and tracing will be natively integrated into
+                    the platform. In just a few clicks, you'll get full
+                    visibility into traffic, errors, and performance across all
+                    services, no extra setup or service reconfiguration needed.
+                  </div>
+                </li>
+
+                <li className="flex items-start gap-3">
+                  <svg
+                    className="w-5 h-5 mt-1 text-blue-800 flex-shrink-0"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      stroke="currentColor"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth="1.2"
+                      d="M15 4h3a1 1 0 0 1 1 1v15a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h3m0 3h6m-3 5h3m-6 0h.01M12 16h3m-6 0h.01M10 3v4h4V3h-4Z"
+                    />
+                  </svg>
+                  <div>
+                    <strong className="font-semibold text-gray-900">
+                      Integrated Dashboard:
+                    </strong>{" "}
+                    Monitor your cluster with the help of a modern looking
+                    dashboard, designed to let users interact with the cluster
+                  </div>
+                </li>
+
+                <li className="flex items-start gap-3">
+                  <svg
+                    className="w-5 h-5 mt-1 text-blue-800 flex-shrink-0"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    fill="currentColor"
+                    viewBox="0 0 24 24"
+                  >
+                    <path
+                      fillRule="evenodd"
+                      d="M8 10V7a4 4 0 1 1 8 0v3h1a2 2 0 0 1 2 2v7a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-7a2 2 0 0 1 2-2h1Zm2-3a2 2 0 1 1 4 0v3h-4V7Zm2 6a1 1 0 0 1 1 1v3a1 1 0 1 1-2 0v-3a1 1 0 0 1 1-1Z"
+                      clipRule="evenodd"
+                    />
+                  </svg>
+                  <div>
+                    <strong className="font-semibold text-gray-900">
+                      Enhanced Network Security:
+                    </strong>{" "}
+                    Secure your cloud-edge cluster with a fast and secure
+                    network packet dropper. CortexBrain makes network security
+                    easy thanks to his underlying network tracker and packet
+                    analyzer
+                  </div>
+                </li>
+              </ul>
             </div>
           </div>
 
           <img
             src="https://www.cortexflow.org/app.png"
             alt="Product screenshot"
-            class="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
             width="2432"
             height="1442"
           />
