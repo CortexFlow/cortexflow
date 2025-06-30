bc36619e983e5f07607f509f11380b7de9a75808
Lorenzo Tettamanti <lorenzo.tettamanti5@gmail.com>
2025-06-30 22:43:55 +0200

    updated navbar with lg/md responsiveness

--- src/components/navbar/navabar.jsx
+++ src/components/navbar/navabar.jsx
@@ -14,6 +14,7 @@ const Homepg = () => {
       <div className=" px-6 bg-gradient-to-r from-gray-900 via-slate-800 to-gray-900 ">
         <div
           className="max-w-7xl mx-auto md:px-2 py-4 flex items-center justify-between
+          lg:px-4
       sm:px-3"
         >
           {/* Logo e nome */}
@@ -25,7 +26,7 @@ const Homepg = () => {
           </Link>
 
           {/* Navbar */}
-          <ul className="font-poppins space-x-8 text-sm font-medium text-gray-300 md:hidden sm:hidden">
+          <ul className="font-poppins space-x-8 text-sm font-medium text-gray-300 md:hidden sm:hidden lg:flex">
             <li>
               <a
                 href="#hero"
@@ -76,7 +77,7 @@ const Homepg = () => {
           <Link
             to="https://github.com/CortexFlow/CortexBrain"
             className="font-poppins hidden items-center bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-lg text-sm font-medium shadow transition duration-300
-            md:hidden sm:hidden"
+            md:hidden sm:hidden lg:inline-flex"
           >
             <img src={githubLogo} alt="GitHub" className="h-4 w-4 mr-2" />
             Get Started
