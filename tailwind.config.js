7c12597c668bfe4e0fd7f33758c8306e3ebcc5f0
Lorenzo Tettamanti <lorenzo.tettamanti5@gmail.com>
2025-07-01 14:21:14 +0200

    added xs screen size

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
+    screens: {
+      xs: "425px",
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {
       fontFamily: {
         poppins: ["Poppins", "sans-serif"],
